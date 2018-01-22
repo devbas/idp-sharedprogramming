4db3a3770f5c0ca19ac8aee4def06b255d6f96af
Bastian Geneugelijk <bastiangeneugelijk@me.com>
2018-01-22 15:18:28 +0100

    Bugfixing

--- app/server/api/resources/code.js
+++ app/server/api/resources/code.js
@@ -10,23 +10,45 @@ router.post('/save', function(req, res) {
     style = req.body.style; 
     script = req.body.script;
     
-    fs.writeFile('./app/client/public/test/index.html', html, function(err) {
-        if(err) console.log('err: ', err) 
+    if(process.env.NODE_ENV === 'production') {
+        fs.writeFile('./app/client/build/test/index.html', html, function(err) {
+            if(err) console.log('err: ', err) 
+    
+            console.log('html on prod saved!');
+        })
+        console.log(path.dirname(__filename));
+        fs.writeFile('./app/client/build/test/script.js', script, function(err) {
+            if(err) console.log('err: ', err) 
+    
+            console.log('script on prod saved!');
+        })
+    
+        fs.writeFile('./app/client/build/test/style.css', style, function(err) {
+            if(err) console.log('err: ', err)  
+    
+            console.log('css on prod saved!');
+        })
+    } else {
+        fs.writeFile('./app/client/public/test/index.html', html, function(err) {
+            if(err) console.log('err: ', err) 
+    
+            console.log('html saved!');
+        })
+        console.log(path.dirname(__filename));
+        fs.writeFile('./app/client/public/test/script.js', script, function(err) {
+            if(err) console.log('err: ', err) 
+    
+            console.log('script saved!');
+        })
+    
+        fs.writeFile('./app/client/public/test/style.css', style, function(err) {
+            if(err) console.log('err: ', err)  
+    
+            console.log('css saved!');
+        })
+    }
 
-        console.log('html saved!');
-    })
-    console.log(path.dirname(__filename));
-    fs.writeFile('./app/client/public/test/script.js', script, function(err) {
-        if(err) console.log('err: ', err) 
-
-        console.log('script saved!');
-    })
-
-    fs.writeFile('./app/client/public/test/style.css', style, function(err) {
-        if(err) console.log('err: ', err)  
-
-        console.log('css saved!');
-    })
+    
 })
 
 module.exports = router; 
